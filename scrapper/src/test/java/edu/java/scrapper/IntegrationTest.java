1dfdbf6dc12c2d86d605a22eeed8b8e3e16cae00
Vlad <martynovq03@gmail.com>
2024-03-09 14:26:24 +0300

    added liquibase test

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,13 +1,35 @@
 package edu.java.scrapper;
 
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.DirectoryResourceAccessor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 @Testcontainers
-public abstract class IntegrationTest {
+public class IntegrationTest {
     public static PostgreSQLContainer<?> POSTGRES;
 
     static {
@@ -17,11 +39,34 @@ public abstract class IntegrationTest {
             .withPassword("postgres");
         POSTGRES.start();
 
-        runMigrations(POSTGRES);
+        try {
+            runMigrations(POSTGRES);
+        } catch (SQLException | FileNotFoundException | LiquibaseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
+    private static void runMigrations(JdbcDatabaseContainer<?> c)
+        throws SQLException, LiquibaseException, FileNotFoundException {
+        Properties properties = new Properties();
+        properties.put("user", c.getUsername());
+        properties.put("password", c.getPassword());
+        Connection connection = DriverManager.getConnection(
+            c.getJdbcUrl(),
+            properties
+        );
+
+        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(
+            new JdbcConnection(connection)
+        );
+
+        Path path = new File(".").toPath().toAbsolutePath().getParent().getParent().resolve("migrations");
+        Liquibase liquibase = new Liquibase(
+            "master.xml",
+            new DirectoryResourceAccessor(path),
+            database
+        );
+        liquibase.update(new Contexts(), new LabelExpression());
     }
 
     @DynamicPropertySource
@@ -29,5 +74,29 @@ public abstract class IntegrationTest {
         registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES::getUsername);
         registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+
+    @Test
+    public void checkDatabase() throws SQLException {
+        Properties properties = new Properties();
+        properties.put("user", POSTGRES.getUsername());
+        properties.put("password", POSTGRES.getPassword());
+        Connection connection = DriverManager.getConnection(
+            POSTGRES.getJdbcUrl(),
+            properties
+        );
+
+        DatabaseMetaData databaseMetaData = connection.getMetaData();
+        ResultSet resultSet = databaseMetaData
+            .getTables(null, null, null, new String[]{"TABLE"});
+        List<String> tables = new ArrayList<>();
+        while (resultSet.next()) {
+            tables.add(resultSet.getString("TABLE_NAME"));
+        }
+
+        Assertions.assertEquals(
+            tables,
+            List.of("chat", "connections", "databasechangelog", "databasechangeloglock", "link")
+        );
     }
 }
